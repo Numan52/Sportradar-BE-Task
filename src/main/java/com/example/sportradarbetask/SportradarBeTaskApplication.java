630238c856abf36cd0c8c5efc33bc7774a54d4d7
Numan52 <numan.demir@gmx.at>
2024-11-08 13:26:36 +0100

    Project initialization

---
+++ src/main/java/com/example/sportradarbetask/SportradarBeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportradarbetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportradarBeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportradarBeTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sportradarbetask/SportradarBeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportradarbetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportradarBeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
