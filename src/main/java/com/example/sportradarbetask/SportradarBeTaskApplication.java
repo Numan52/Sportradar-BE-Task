83bd082a85fbf7dc948d83c20ec8f8598a3f2f83
Numan52 <numan.demir@gmx.at>
2024-11-10 11:25:01 +0100

    made database credentials environment variables

--- src/main/java/com/example/sportradarbetask/SportradarBeTaskApplication.java
+++ src/main/java/com/example/sportradarbetask/SportradarBeTaskApplication.java
@@ -1,12 +1,19 @@
 package com.example.sportradarbetask;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class SportradarBeTaskApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
+		System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+
 		SpringApplication.run(SportradarBeTaskApplication.class, args);
 	}
 
